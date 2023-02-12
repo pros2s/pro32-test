a8ca205595d6951577e3403a2fc08067d8db9abb
Alexandr Podoplelov <caiiiia_podoplelov@mail.ru>
2023-02-12 03:04:12 +0300

    strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import { App } from 'pages/App';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.less';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
